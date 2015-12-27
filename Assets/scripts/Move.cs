1a7327ae8c06b055b9fc1f13716fcaeec7e54bd9
Aashish Karki <aashish@clypd.com>
2015-12-27 12:36:15 -0500

    Initial commit

---
+++ Assets/scripts/Move.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public float speed = 2.0f;
+
+	void Update() {
+		var move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+		transform.position += move * speed * Time.deltaTime;
+	}
+}
