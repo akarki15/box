f07fc249d2c14face1135e7934f20249aa0c2906
Aashish Karki <aashish@clypd.com>
2015-12-29 22:42:04 -0500

    Minimal box gameplay
    - Box dies after it hits floor the second time

--- Assets/scripts/Move.cs
+++ Assets/scripts/Move.cs
@@ -4,9 +4,20 @@ using System.Collections;
 public class Move : MonoBehaviour {
 
 	public float speed = 2.0f;
-
+	public bool gameOver=false;
 	void Update() {
-		var move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-		transform.position += move * speed * Time.deltaTime;
+		if (!gameOver) {
+			var move = new Vector3 (Input.GetAxis ("Horizontal"), 0, 0);
+			transform.position += move * speed * Time.deltaTime;
+
+			if (Input.touchCount == 1) {
+				var touch = Input.touches [0];
+				if (touch.position.x < Screen.width / 2) {
+					transform.position -= new Vector3 (1f, 0, 0) * speed * Time.deltaTime;
+				} else if (touch.position.x > Screen.width / 2) {
+					transform.position += new Vector3 (1f, 0, 0) * speed * Time.deltaTime;
+				}
+			}
+		}
 	}
 }
