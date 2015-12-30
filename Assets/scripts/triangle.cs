39209f7870da51a78b02307f7bbde8f24258576c
Aashish Karki <aashish@clypd.com>
2015-12-29 23:31:57 -0500

    Debugs time in air at the end of the gameplay

---
+++ Assets/scripts/triangle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triangle : MonoBehaviour {
+	public bool startGame;
+	// Use this for initialization
+	public GameObject floor;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision col) {
+		if (col.gameObject.name == "box"  && !startGame) {
+			startGame = true;
+			floor.GetComponent<gameplay>().startTime = Time.time;
+			Debug.Log (floor.GetComponent<gameplay> ().startTime);
+		} 
+	}
+
+}
