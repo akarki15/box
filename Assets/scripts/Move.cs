24be7bfff46c2779bf8e37bd0e6732e5615799f6
Aashish Karki <aashish@clypd.com>
2015-12-29 22:10:45 -0500

    New assets

--- Assets/scripts/Move.cs
+++ Assets/scripts/Move.cs
@@ -2,12 +2,44 @@
 using System.Collections;
 
 public class move : MonoBehaviour {
+	public float speed = 1.0f;
+	public bool debugMode;
+	public int strength;
 
-	public float speed = 2.0f;
+	void Start(){
+		debugMode = true;
+	}
+
+
 
 	void Update() {
-		var move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-		transform.position += move * speed * Time.deltaTime;
+		if (Input.touchCount == 1)
+		{
+			var touch = Input.touches[0];
+			if (touch.position.x < Screen.width/2)
+			{
+				transform.position -= new Vector3(1f, 0, 0) * speed * Time.deltaTime;
+			}
+			else if (touch.position.x > Screen.width/2)
+			{
+				transform.position += new Vector3(1f, 0, 0) * speed * Time.deltaTime;
+			}
+		}
+		if (debugMode){
+			if (Input.GetAxis ("Horizontal") > 0) {
+				Debug.Log ("right");
+				Vector3 dir = new Vector3 (100f, 0f, 0f);
+				dir.Normalize ();
+				this.gameObject.GetComponent<Rigidbody>().AddForce (dir*10);
+	
+			} else if (Input.GetAxis ("Horizontal"q) < 0) {
+				Debug.Log ("left");
+				Vector3 dir = new Vector3 (-100f, 0f, 0f);
+				dir.Normalize ();
+				this.gameObject.GetComponent<Rigidbody>().AddForce (dir*10);
+			}
+		}
+
 	}
 		
 }
